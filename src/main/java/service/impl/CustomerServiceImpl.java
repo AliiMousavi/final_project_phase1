4477c6d36921627eebfe575fd111a3d489a96a55
AliiMousavi <AliMousavi04897@gmail.com>
2023-10-13 06:38:54 +0330

    implement CustomerServiceImpl.java

---
+++ src/main/java/service/impl/CustomerServiceImpl.java
@@ -0,0 +1,29 @@
+package service.impl;
+
+import base.service.BaseServiceImpl;
+import entity.users.Customer;
+import entity.users.Expert;
+import repository.CustomerRepository;
+import service.CustomerService;
+import util.Validator;
+
+public class CustomerServiceImpl
+        extends BaseServiceImpl<Customer,Long, CustomerRepository>
+        implements CustomerService {
+    public CustomerServiceImpl(CustomerRepository repository) {
+        super(repository);
+    }
+
+
+    @Override
+    public Customer ChangePasswordByID(String password, Long id) {
+        Customer customer = repository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("expert not found with id: " + id));
+        if(Validator.isValidPassword(password))
+            customer.setPassword(password);
+        else
+            throw new RuntimeException("is not valid password!");
+
+        return repository.saveOrUpdate(customer);
+    }
+}
