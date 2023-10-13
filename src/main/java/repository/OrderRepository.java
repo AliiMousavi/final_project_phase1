60576f4a1887cd3acd8904d4c277507739401821
AliiMousavi <AliMousavi04897@gmail.com>
2023-10-13 06:59:13 +0330

    Implement OrderRepository.java

---
+++ src/main/java/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package repository;
+
+import base.repository.BaseRepository;
+import entity.Order;
+import entity.Services.ServiceCollection;
+
+public interface OrderRepository extends BaseRepository<Order,Long> {
+
+}
