a083b9d2bcebaa645ae6c535fc2dce70d22a470b
AliiMousavi <AliMousavi04897@gmail.com>
2023-10-13 06:42:17 +0330

    Implement ServiceCollectionRepository.java

---
+++ src/main/java/repository/ServiceCollectionRepository.java
@@ -0,0 +1,9 @@
+package repository;
+
+import base.repository.BaseRepository;
+import entity.Services.ServiceCollection;
+
+import java.nio.file.LinkOption;
+
+public interface ServiceCollectionRepository extends BaseRepository<ServiceCollection,Long> {
+}
