d4b72c0c2193af2e45e2a38932df3c2e28475465
AliiMousavi <AliMousavi04897@gmail.com>
2023-10-13 06:40:49 +0330

    Implement Order.java

---
+++ src/main/java/entity/Order.java
@@ -0,0 +1,34 @@
+package entity;
+
+import base.entity.BaseEntity;
+import entity.Services.SubServiceCollection;
+import entity.enums.OrderStatus;
+import entity.users.Customer;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import java.sql.Date;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "orders")
+public class Order extends BaseEntity<Long>{
+    int offerPrice;
+    String workToDo;
+    Date DateOfExecution;
+    String address;
+    OrderStatus orderStatus;
+    @OneToOne
+    Customer customer;
+    @OneToOne
+    SubServiceCollection subServiceCollection;
+    @OneToOne
+    Offer offer;
+
+}
