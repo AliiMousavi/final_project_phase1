714e51b187367ed0f2c77e6158724c3ea59b473a
AliiMousavi <AliMousavi04897@gmail.com>
2023-10-13 06:58:21 +0330

    add Ordering method to CustomerService.java

--- src/main/java/service/CustomerService.java
+++ src/main/java/service/CustomerService.java
@@ -1,8 +1,10 @@
 package service;
 
 import base.service.BaseService;
+import entity.Order;
 import entity.users.Customer;
 
 public interface CustomerService extends BaseService<Customer,Long> {
     Customer ChangePasswordByID(String password,Long id);
+    Order Ordering(Order order);
 }
