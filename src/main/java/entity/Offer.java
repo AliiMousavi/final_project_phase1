65313e5f83e46cad6b74a0dc5b908d8c30f227a0
AliiMousavi <AliMousavi04897@gmail.com>
2023-10-13 06:40:40 +0330

    Implement Offer.java

---
+++ src/main/java/entity/Offer.java
@@ -0,0 +1,29 @@
+package entity;
+
+import base.entity.BaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import java.sql.Date;
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "offers")
+public class Offer extends BaseEntity<Long> {
+    Date DateOfTheOffer;
+    int offeredprice;
+    Date offeredDate;
+    int Duration;
+    @OneToOne(mappedBy = "offer")
+    Order order;
+
+
+}
