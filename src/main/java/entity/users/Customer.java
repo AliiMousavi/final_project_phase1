bc68b7e83fb03d0e6b94f5fbff407bad57208add
AliiMousavi <AliMousavi04897@gmail.com>
2023-10-13 06:38:16 +0330

    implement Customer.java

--- src/main/java/entity/users/Customer.java
+++ src/main/java/entity/users/Customer.java
@@ -1,8 +1,33 @@
 package entity.users;
 
+import entity.Order;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
+import javax.persistence.OneToOne;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Customer extends User{
+    @OneToOne(mappedBy = "customer")
+    Order order;
 
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", id=" + id +
+                '}';
+    }
+
+    public Customer(String firstName, String lastName, String email) {
+        super(firstName, lastName, email);
+    }
 }
