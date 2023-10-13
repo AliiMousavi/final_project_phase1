1a1e8fa09645f5c75f2e8aa4bd5d0d914fee8845
AliiMousavi <AliMousavi04897@gmail.com>
2023-10-13 06:39:55 +0330

    Implement Expert.java

--- src/main/java/entity/users/Expert.java
+++ src/main/java/entity/users/Expert.java
@@ -1,16 +1,40 @@
 package entity.users;
 
+import entity.Comment;
+import entity.Services.SubServiceCollection;
 import entity.enums.ExpertStatus;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Setter
+@Getter
 public class Expert extends User {
-    ExpertStatus exportStatus;
+    ExpertStatus expertStatus;
+
+    @ManyToMany(mappedBy = "Experts" , cascade = CascadeType.PERSIST)
+    List<SubServiceCollection> subServiceCollections;
+
+    @OneToMany(mappedBy = "expert")
+    List<Comment> comments;
+
+    @Override
+    public String toString() {
+        return "Expert{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", id=" + id +
+                '}';
+    }
+
+    public Expert(String firstName, String lastName, String email) {
+        super(firstName, lastName, email);
+    }
 }
