9d3a505f8e8663992bbc6af53fbdbaa5d9cd7fe6
AliiMousavi <AliMousavi04897@gmail.com>
2023-10-13 06:58:29 +0330

    add Ordering method to CustomerServiceImpl.java

--- src/main/java/service/impl/CustomerServiceImpl.java
+++ src/main/java/service/impl/CustomerServiceImpl.java
@@ -1,10 +1,12 @@
 package service.impl;
 
 import base.service.BaseServiceImpl;
+import entity.Order;
 import entity.users.Customer;
 import entity.users.Expert;
 import repository.CustomerRepository;
 import service.CustomerService;
+import util.ApplicationContext;
 import util.Validator;
 
 public class CustomerServiceImpl
@@ -25,5 +27,10 @@ public class CustomerServiceImpl
             throw new RuntimeException("is not valid password!");
 
         return repository.saveOrUpdate(customer);
+    }
+
+    @Override
+    public Order Ordering(Order order) {
+        return ApplicationContext.getOrderService().saveOrUpdate(order);
     }
 }
