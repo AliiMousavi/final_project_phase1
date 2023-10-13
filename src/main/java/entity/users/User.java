8283414b6d7beab58e372d77a7cf91124653bb8f
AliiMousavi <AliMousavi04897@gmail.com>
2023-10-13 06:44:21 +0330

    Add constructor to User.java

--- src/main/java/entity/users/User.java
+++ src/main/java/entity/users/User.java
@@ -1,20 +1,26 @@
 package entity.users;
 
 import base.entity.BaseEntity;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import util.PasswordGenerator;
+import util.Validator;
 
 import javax.persistence.*;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.sql.Date;
+import java.time.LocalDate;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Getter
+@Setter
 @Table(name = "Users")
-public abstract class User extends BaseEntity<Long>{
+public abstract class User extends BaseEntity<Long> {
     String firstName;
     String lastName;
     @Column(unique = true)
@@ -25,4 +31,52 @@ public abstract class User extends BaseEntity<Long>{
     @Lob
     @Column(name = "image")
     private byte[] image;
+    int credit;
+
+    public User(String firstName, String lastName, String email, byte[] image) throws IOException {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        if (Validator.isValidEmail(email))
+            this.email = email;
+        else
+            throw new RuntimeException("email is not valid!");
+        this.password = PasswordGenerator.generatePassword();
+        this.dateOfRegistration = Date.valueOf(LocalDate.now());
+        this.credit = 0;
+
+        if (Validator.isValidImage(convertByteArrayToFile(image)))
+            this.image = image;
+
+    }
+
+    public User(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        if (Validator.isValidEmail(email))
+            this.email = email;
+        else
+            throw new RuntimeException("email is not valid!");
+        this.password = PasswordGenerator.generatePassword();
+        this.dateOfRegistration = Date.valueOf(LocalDate.now());
+        this.credit = 0;
+    }
+
+    private static File convertByteArrayToFile(byte[] byteArray) throws IOException {
+        File file = new File("\\Desktop");
+        FileOutputStream fos = new FileOutputStream(file);
+        fos.write(byteArray);
+        fos.close();
+        return file;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", id=" + id +
+                '}';
+    }
 }
