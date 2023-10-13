b20206b3a1e305bc9921644e5a097acff4225f30
AliiMousavi <AliMousavi04897@gmail.com>
2023-10-13 06:41:02 +0330

    Implement OrderStatus.java

---
+++ src/main/java/entity/enums/OrderStatus.java
@@ -0,0 +1,9 @@
+package entity.enums;
+
+public enum OrderStatus {
+    WAITING_FOR_THE_SUGGESTION_OF_EXPERTS,
+    WAITING_FOR_SELECT_NEXPERT,
+    WAITING_FOR_THE_EXPERT_TO_COME_TO_YOUR_PLACE,
+    DONE,
+    PAID
+}
