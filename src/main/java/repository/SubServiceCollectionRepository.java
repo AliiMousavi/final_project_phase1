beee6befeafd48b222e692d51326bab1dc081fc9
AliiMousavi <AliMousavi04897@gmail.com>
2023-10-13 06:42:50 +0330

    Implement SubServiceCollectionRepository.java

---
+++ src/main/java/repository/SubServiceCollectionRepository.java
@@ -0,0 +1,9 @@
+package repository;
+
+import base.repository.BaseRepository;
+import com.sun.xml.bind.v2.model.core.ID;
+import entity.Services.SubServiceCollection;
+
+public interface SubServiceCollectionRepository extends BaseRepository<SubServiceCollection,Long> {
+
+}
