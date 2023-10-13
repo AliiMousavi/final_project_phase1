02847fdd94234508a0c14e7d2f96ea4fd53b0f78
AliiMousavi <AliMousavi04897@gmail.com>
2023-10-13 06:37:52 +0330

    implement ApplicationContext.java

--- src/main/java/util/ApplicationContext.java
+++ src/main/java/util/ApplicationContext.java
@@ -1,13 +1,79 @@
 package util;
 
 import config.EntityManagerProvider;
+import entity.users.User;
+import repository.*;
+import repository.impl.*;
+import service.*;
+import service.impl.*;
 
 import javax.persistence.EntityManager;
 
 public class ApplicationContext {
     private static final EntityManager em;
 
+    private static final UserRepository<User> userRepository;
+    private static final UserService<User> userService;
+
+    private static final AdminRepository adminRepository;
+    private static final AdminService adminService;
+
+    private static final CustomerRepository customerRepository;
+    private static final CustomerService customerService;
+
+    private static final ExpertRepository expertRepository;
+    private static final ExpertService expertService;
+
+    private static final ServiceCollectionRepository serviceCollectionRepository;
+    private static final ServiceCollectionService serviceCollectionService;
+
+    private static final SubServiceCollectionRepository subServiceCollectionRepository;
+    private static final SubServiceCollectionService subServiceCollectionService;
+
+
+
+
     static {
         em = EntityManagerProvider.getEntityManager();
+        userRepository = new UserRepositoryImpl<>(em) {
+            @Override
+            public Class<User> getEntityClass() {
+                return User.class;
+            }
+        };
+        userService = new UserServiceImpl<>(userRepository);
+        adminRepository= new AdminRepositoryImpl(em);
+        adminService=new AdminServiceImpl(adminRepository);
+        customerRepository= new CustomerRepositoryImpl(em);
+        customerService=new CustomerServiceImpl(customerRepository);
+        expertRepository= new ExpertRepositoryImpl(em);
+        expertService= new ExpertServiceImpl(expertRepository);
+        serviceCollectionRepository=new ServiceCollectionRepositoryImpl(em);
+        serviceCollectionService= new ServiceCollectionServiceImpl(serviceCollectionRepository);
+        subServiceCollectionRepository= new SubServiceCollectionRepositoryImpl(em);
+        subServiceCollectionService= new SubServiceCollectionServiceImpl(subServiceCollectionRepository);
+
     }
+
+    public static UserService<User> getUserService(){
+        return userService;
+    }
+
+    public static AdminService getAdminService(){
+        return adminService;
+    }
+
+    public static CustomerService getCustomerService(){
+        return customerService;
+    }
+    public static ExpertService getExpertService(){
+        return expertService;
+    }
+    public static ServiceCollectionService getServiceCollectionService(){
+        return serviceCollectionService;
+    }
+    public static SubServiceCollectionService getSubServiceCollectionService(){
+        return subServiceCollectionService;
+    }
+
 }
