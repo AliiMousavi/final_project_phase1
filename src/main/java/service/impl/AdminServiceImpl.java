5bef429be6606c677e3ec6a3f81e88cd3aec9435
AliiMousavi <AliMousavi04897@gmail.com>
2023-11-23 18:50:32 +0330

    implement AdminService.addExpertToSubServiceCollection()

--- src/main/java/service/impl/AdminServiceImpl.java
+++ src/main/java/service/impl/AdminServiceImpl.java
@@ -3,12 +3,17 @@ package service.impl;
 import base.service.BaseServiceImpl;
 import entity.Services.ServiceCollection;
 import entity.Services.SubServiceCollection;
+import entity.enums.ExpertStatus;
 import entity.users.Admin;
+import entity.users.Expert;
 import repository.AdminRepository;
 import service.AdminService;
 import util.ApplicationContext;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
 
 public class AdminServiceImpl extends UserServiceImpl<Admin> implements AdminService {
     public AdminServiceImpl(AdminRepository repository) {
@@ -44,6 +49,30 @@ public class AdminServiceImpl extends UserServiceImpl<Admin> implements AdminSer
     @Override
     public SubServiceCollection updateCaption(Long subServiceId, String newCaption) {
         return ApplicationContext.getSubServiceCollectionService().updateCaption(subServiceId,newCaption);
+    }
+
+    @Override
+    public SubServiceCollection addExpertToSubServiceCollection(Expert expert ,SubServiceCollection SubServiceCollection ) {
+        SubServiceCollection byId = ApplicationContext.getSubServiceCollectionService().findById(SubServiceCollection.getId())
+                .orElseThrow(() -> new IllegalArgumentException("Sub-service not found with id: " + SubServiceCollection.getId()));
+        List<Expert> experts = new ArrayList<>();
+        experts.add(expert);
+        byId.setExperts(experts);
+       return ApplicationContext.getSubServiceCollectionService().saveOrUpdate(byId);
+    }
+
+    @Override
+    public SubServiceCollection deleteExpertToSubServiceCollection(Expert expert, SubServiceCollection SubServiceCollection) {
+        SubServiceCollection.getExperts().remove(expert);
+       return ApplicationContext.getSubServiceCollectionService().saveOrUpdate(SubServiceCollection);
+    }
+
+    @Override
+    public Expert confirmExpertbyId(Long id) {
+        Expert byId = ApplicationContext.getExpertService().findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Sub-service not found with id: " + id));;
+        byId.setExpertStatus(ExpertStatus.ACCEPTED);
+        return ApplicationContext.getExpertService().saveOrUpdate(byId);
     }
 
 }
